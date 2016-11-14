cd4ae11ee94fe2821a8bc4a0a7db5082e829d6d9
fhergomez <fhergomez37@gmail.com>
2016-11-14 00:38:44 -0800

    added gulp for gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ var ghPages      = require('gulp-gh-pages');
  */
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./build/**/*')
     .pipe(ghPages());
 });
 
