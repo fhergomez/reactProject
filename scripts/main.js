72a0a88a25d956bb96ce8b886019f7a4ed7f35d0
fhergomez <fhergomez37@gmail.com>
2016-11-13 23:43:47 -0800

    add helper.js function to StorePicker component

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,11 @@ var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 /*
+  requirin helpers.js file
+*/
+var helpers = require('./helpers');
+
+/*
   App
 */
 
@@ -80,7 +85,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" defaultValue = {helpers.getFunName()} required />
         <input type="Submit" />
       </form>
     )
