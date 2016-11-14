e41a5ced4c8e95553284a6df1d16fef2454b05c8
fhergomez <fhergomez37@gmail.com>
2016-11-13 23:20:49 -0800

    add routes

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,105 @@
 var React = require('react');
-var ReactDOM = require('react-dom')
+var ReactDOM = require('react-dom');
+
+var ReactRouter = require('react-router')
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 /*
-  StorePicker
+  App
+*/
+
+var App = React.createClass({
+  render : function () {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/*
+  Header Component
+*/
+var Header = React.createClass({
+  render : function () {
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>Day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+});
+
+/*
+  Order Component
+  <Order/>
+*/
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+  Inventory Component
+  <Inventory/>
+*/
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+
+/*
+  StorePicker Component
   This will let us make <StorePicker/>
 */
 
 var StorePicker = React.createClass({
-  render : function () {
+
+  render : function() {
     return (
-      <form className = "store-selector">
-        <h2>Please enter a store</h2>
-        <input type="text" ref="storeId" required/>
-        <input type="submit"/>
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" required />
+        <input type="Submit" />
       </form>
     )
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+/*
+  Not Found Component
+*/
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
